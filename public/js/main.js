3549e7b8267040193358305e714a0217ee3fc61d
gustavo-tp <gustavo1190.gt@gmail.com>
2019-12-25 17:00:24 -0300

    Added keyboard input listener and functions to add traits in mario like jump and velocity.

--- public/js/main.js
+++ public/js/main.js
@@ -1,43 +1,52 @@
 import Compositor from './Compositor.js';
+import Timer from './Timer.js';
 import { loadLevel } from './loaders.js';
-import { loadMarioSprite, loadBackgroundSprites } from './sprites.js';
-import { createBackgrounLayer } from './layers.js';
+import { createMario } from './entities.js';
+import { loadBackgroundSprites } from './sprites.js';
+import { createBackgrounLayer, createSpriteLayer } from './layers.js';
+
+import Keyboard from './KeyboardState.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-function createSpriteLayer(sprites, pos) {
-    return function drawSpriteLayer(context) {
-        for (let i = 0; i < 20; ++i) {
-            sprites.draw('idle', context, pos.x + i * 16, pos.y);
-        }
-    };
-}
-
 Promise.all([
-    loadMarioSprite(),
+    createMario(),
     loadBackgroundSprites(),
     loadLevel('1-1')
-]).then(([marioSprite, backgroundSprites, level]) => {
+]).then(([mario, backgroundSprites, level]) => {
     const comp = new Compositor();
 
     const drawBackgroundLayer = createBackgrounLayer(level.backgrounds, backgroundSprites);
     comp.layers.push(drawBackgroundLayer);
 
-    const pos = {
-        x: 0,
-        y: 0
-    };
+    const gravity = 2000;
+    mario.pos.set(64, 180);
+    //mario.vel.set(200, -600);
 
-    const spriteLayer = createSpriteLayer(marioSprite, pos);
+    const SPACE = 32;
+    const input = new Keyboard();
+    input.addMapping(SPACE, keyState => {
+        if (keyState) {
+            mario.jump.start();
+        } else {
+            mario.jump.cancel();
+        }
+        console.log(keyState);
+    });
+    input.listenTo(window);
+
+    const spriteLayer = createSpriteLayer(mario);
     comp.layers.push(spriteLayer);
 
-    function update() {
+    const timer = new Timer(1/60);
+    timer.update =  function update(deltaTime) {
+        mario.update(deltaTime);
+
         comp.draw(context);
-        pos.x += 2;
-        pos.y += 2;
-        requestAnimationFrame(update);
+
+        mario.vel.y += gravity * deltaTime;
     }
 
-    update();
+    timer.start();
 });
